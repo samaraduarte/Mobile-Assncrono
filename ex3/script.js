e42030616782981e2b9db3ebe3d741514fbd2869
samara <samduarte1234@gmail.com>
2020-04-01 22:35:36 -0300

    Adicionando arquivos

--- ex3/script.js
+++ ex3/script.js
@@ -1,3 +1,3 @@
 function carregar(){
-    innerHTML = '<img scr="https://media.giphy.com/media/11ASZtb7vdJagM/giphy.gif">'
+    document.getElementById("paragrafo").innerHTML = "<img scr='https://img.ibxk.com.br/2014/3/materias/4805475817181134.gif'>"
 }
